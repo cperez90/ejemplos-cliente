28b4558decb89c1d337c075ac8b2ed593158481d
riku <cperez90@alumnes.politecnicllevant.cat>
2025-12-12 11:38:08 +0000

    vue

--- node-vite/primer-ejemplo-vue/primer-vue/src/views/HelloWorldView.vue
+++ node-vite/primer-ejemplo-vue/primer-vue/src/views/HelloWorldView.vue
@@ -6,10 +6,12 @@ const x = ref('hello world');
 /*
 1) Vue actualiza la variable a tod0 el component
  */
+const activeColor = ref('red')
 </script>
 
 <template>
-<h1>{{x}}</h1>
+<h1 v-if="x==='hola'" :style="{color: activeColor}">{{x}}</h1>
+  <h1 v-else>{{x}}</h1>
   <p v-if="x==='adeu'">Adeu, se ha acabado la clase</p>
   <input v-model="x">
 
