77f00e7d48034de7ca1677b2a662f4cd3a506a35
riku <cperez90@alumnes.politecnicllevant.cat>
2025-12-19 11:54:40 +0000

    vue

---
+++ node-vite/primer-ejemplo-vue/primer-vue/src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+<header>
+  <h1>Welcome</h1>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Home</li>
+      <li>Home</li>
+      <li>Home</li>
+      <li>Home</li>
+    </ul>
+  </nav>
+</header>
+</template>
+
+<style scoped>
+h1{
+  font-size: 2rem;
+  text-align: center;
+  margin: 20px 0;
+  color: blueviolet;
+}
+</style>
--- node-vite/primer-ejemplo-vue/primer-vue/src/views/HelloWorldView.vue
+++ node-vite/primer-ejemplo-vue/primer-vue/src/views/HelloWorldView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {ref} from "vue";
-
+import Header from "../components/Header.vue";
 const x = ref('hello world');
 //Reactiva
 /*
@@ -10,13 +10,15 @@ const activeColor = ref('red')
 </script>
 
 <template>
-<h1 v-if="x==='hola'" :style="{color: activeColor}">{{x}}</h1>
-  <h1 v-else>{{x}}</h1>
-  <p v-if="x==='adeu'">Adeu, se ha acabado la clase</p>
-  <input v-model="x">
-
+<!--<h1 v-if="x==='hola'" :style="{color: activeColor}">{{x}}</h1>-->
+<h1 :class="{vermell: (x === 'hola')}">{{x}}</h1>
+<p v-if="x==='adeu'">Adeu, se ha acabado la clase</p>
+<input v-model="x">
+<Header/>
 </template>
 
 <style scoped>
-
+.vermell{
+  color: red;
+}
 </style>
